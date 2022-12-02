f423598973ea0b4854cbd10db365362ee93d7a6c
teraglass <67316314+teraglass@users.noreply.github.com>
2022-12-02 16:36:58 +0900

    model 추가

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+import numpy as np
+import tensorflow as tf
 
 app = Flask(__name__)
 
+model_path = './model.h5'
+model = tf.saved_model(model_path)
 
 @app.route('/')
 def index():  # put application's code here
     return render_template('index.html')
+
+@app.route("predict")
+def predict():
+    pass
 
 @app.route('/result')
 def result():
